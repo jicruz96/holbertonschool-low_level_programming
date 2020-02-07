38e5127e6fe156c6b1dfcc815bce9571026c355d
jicruz96 <accounts@jicruz.com>
2020-02-07 06:00:32 +0000

    Push ex. 8 to repo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c, b;
+
+	c = '48';
+	b = '97'
+	while (c <= '57')
+	{
+		putchar (c);
+		c++;
+	}
+	while (b <= 102)
+	{
+		putchar (b);
+		b++;
+	}
+	putchar ('\n');
+	return (0);
+}
