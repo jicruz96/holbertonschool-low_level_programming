72fa860a36f3f8b2d0020e6392ba552c06cbb5cc
jicruz96 <accounts@jicruz.com>
2020-02-10 17:23:33 +0000

    Push adv. ex. 12 to repo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	x = '0';
+	while (x <= '7')
+	{
+		y = x + 1;
+		{
+			while (y <= '8')
+			{
+				z = y + 1;
+				while (z <= '9')
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x == '7' && y == '8' && z == '9')
+						break;
+					putchar(',');
+					putchar(' ');
+					z++;
+				}
+				y++;
+			}
+		x++;
+		}
+		putchar('\n');
+	}
+	return (0);
+}
