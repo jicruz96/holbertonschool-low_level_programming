c8df648430eaca24c7d792c07546b57f9391e961
jicruz96 <accounts@jicruz.com>
2020-02-11 15:27:43 +0000

    Debug ex. 1, 1st attempt: Alter documentation syntax

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,12 @@
 
 /**
 * print_alphabet - prints lower case alphabet to stdout
+* 
+* Description: 
+* prints all lower case letters in alphabetical order
+* to stdout out once, followed by a new line.
+* 
+* Return: none-- void function
 **/
 
 void print_alphabet (void)
