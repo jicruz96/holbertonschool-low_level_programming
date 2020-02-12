d5b0590c5c1b0ce9fb11a860d0a3d9208ae2eabc
jicruz96 <accounts@jicruz.com>
2020-02-12 05:53:44 +0000

    Add functions to holberton header

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -20,4 +20,22 @@ void positive_or_negative(int n);
 
 int largest_number(int a, int b, int c);
 
+/**
+* print_remaining_days - prints remaining days in year from starting point
+* @month: month of the year (1-12)
+* @day: day of the month (1-31)
+* @year: a year (any value)
+*/
+
+void print_remaining_days(int month, int day, int year);
+
+/**
+* convert_day - turns day of month into day of year
+* @month: month of the year (1-12)
+* @day: day of the month (1-31)
+* Return: day of the year
+*/
+
+int convert_day(int month, int day);
+
 #endif
