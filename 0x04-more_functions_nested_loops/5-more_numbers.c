f13ff7096965480a5093252c8ac5ad0fb05ee26b
jicruz96 <accounts@jicruz.com>
2020-02-13 18:19:08 +0000

    Debug ex. 5, 1st attempt: add for loop to print result 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,16 @@
 
 void more_numbers(void)
 {
-	int n;
+	int n, lines;
 
-	for (n = 0; n < 15; n++)
+	for (lines = 0; lines < 10; lines++)
 	{
-		if (n > 9)
-			_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 9)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
-
