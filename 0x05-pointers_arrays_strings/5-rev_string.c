5c5529bb425c126c0cb740eb1f45fb39f7bd950f
jicruz96 <accounts@jicruz.com>
2020-02-19 04:06:48 +0000

    Push completed ex. 7 to repo

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,34 +11,18 @@
 
 void rev_string(char *s)
 {
-	int len = _strlen(s) - 1;
+	int strlen = 0;
 	int index_num;
 	char *reverse;
 
-	for (index_num = 0; len >= 0; len--, index_num++)
+	while (s[strlen])
+		strlen++;
+	strlen--;
+	for (index_num = 0; strlen >= 0; strlen--, index_num++)
 	{
-		reverse[len] = s[index_num];
+		reverse[strlen] = s[index_num];
 		/*printf("Value of reverse: %s\n", reverse);
 		printf("Value of count: %d\n", count);
 		printf("Value of index_num: %d\n", index_num);*/
 	}
-	printf("Value of *reverse: %c\n", *reverse);
-	printf("Value of reverse: %s\n", reverse);
-}
-
-/**
-* _strlen - returns the length of a string
-*
-* @s: string to be evaluated
-*
-* Return: integer representing length of string
-*/
-
-int _strlen(char *s)
-{
-	int strlen;
-
-	while (s[strlen])
-		strlen++;
-	return (strlen);
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
 * puts_half - prints the second half of a string
 *
@@ -10,5 +10,15 @@
 
 void puts_half(char *str)
 {
-	/* code goes here */
+	int strlen = 0;
+	int n, c;
+
+	while (str[strlen])
+		strlen++;
+	for (n = (strlen + 1) / 2; n <= strlen; n++)
+	{
+		c = str[n];
+		_putchar(c);
+	}
+	_putchar(10);
 }
