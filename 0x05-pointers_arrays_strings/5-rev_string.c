1b5850401e32eecf95d535d71892f6e5999c6e76
jicruz96 <accounts@jicruz.com>
2020-02-19 03:01:09 +0000

    Push completed ex. 6 to repo

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
 * rev_string - reverses a string
@@ -10,5 +11,34 @@
 
 void rev_string(char *s)
 {
-	/* code goes here */
+	int len = _strlen(s) - 1;
+	int index_num;
+	char *reverse;
+
+	for (index_num = 0; len >= 0; len--, index_num++)
+	{
+		reverse[len] = s[index_num];
+		/*printf("Value of reverse: %s\n", reverse);
+		printf("Value of count: %d\n", count);
+		printf("Value of index_num: %d\n", index_num);*/
+	}
+	printf("Value of *reverse: %c\n", *reverse);
+	printf("Value of reverse: %s\n", reverse);
+}
+
+/**
+* _strlen - returns the length of a string
+*
+* @s: string to be evaluated
+*
+* Return: integer representing length of string
+*/
+
+int _strlen(char *s)
+{
+	int strlen;
+
+	while (s[strlen])
+		strlen++;
+	return (strlen);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,5 +10,12 @@
 
 void puts2(char *str)
 {
-	/* code goes here */
+	int index_num, c;
+
+	for (index_num = 0; str[index_num]; index_num+= 2)
+	{
+		c = str[index_num];
+		_putchar(c);
+	}
+	_putchar(10);
 }
