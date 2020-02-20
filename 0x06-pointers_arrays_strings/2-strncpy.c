feb1a329116daaa18896a694b60afdaecb5981ac
jicruz96 <accounts@jicruz.com>
2020-02-20 22:31:35 +0000

    Push ex. 2 to repo

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* _strncpy - copies a string value, up to the nth byte, onto another string
+*
+* @src: string to be copied-- up to src's nth byte
+* @dest: string that src is copied into, starting from the beginning
+* @n: number of bytes of src to be copied to dest
+*
+* Return: charpointer to copied string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+}
