d942e9edb65c35fea804ea943cd962e2df492e39
jicruz96 <accounts@jicruz.com>
2020-02-21 22:36:38 +0000

    Debug ex. 101, 2nd attempt: previous attempt had unnecessary printfs

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -2,13 +2,12 @@
 
 int main(void)
 {
-	int n, l;
+	int n;
 	int a[5];
 	int *p;
 
 	p = &n;
 	a[2] = 1024;
-
 	/*
 	* write your line of code here...
 	* Remember:
@@ -18,13 +17,7 @@ int main(void)
 	* - you are not allowed to code anything else than this line of code
 	*/
 	*(p + 6) = 98;
-	printf("n = %p\n", &n);
-	printf("l = %p\n", &l);
-	printf("p = %p\n", &p);
-	for (l = 0; l < 5; l++)
-	{
-		/* ...so that this prints 98\n */
-		printf("a[%d] = %d and is in %p\n", l, a[l], &a[l]);
-	}
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
