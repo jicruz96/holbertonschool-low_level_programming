f5fb223a496bdc0ac1616f7b730d89683ed3f3f8
jicruz96 <accounts@jicruz.com>
2020-02-21 22:34:09 +0000

    Debug ex. 101, 1st attempt

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -2,22 +2,29 @@
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n, l;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p - 4) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	p = &n;
+	a[2] = 1024;
+
+	/*
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
+	*/
+	*(p + 6) = 98;
+	printf("n = %p\n", &n);
+	printf("l = %p\n", &l);
+	printf("p = %p\n", &p);
+	for (l = 0; l < 5; l++)
+	{
+		/* ...so that this prints 98\n */
+		printf("a[%d] = %d and is in %p\n", l, a[l], &a[l]);
+	}
+	return (0);
 }
