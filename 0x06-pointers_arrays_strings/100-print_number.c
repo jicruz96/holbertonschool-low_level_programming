0611d62aa42f3c614b50c99765e884e87e5e9884
jicruz96 <accounts@jicruz.com>
2020-02-21 04:09:44 +0000

    Push ex. 100 to repo

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,42 @@
+#include <math.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* print_number - prints integer n
+* @n: number to be printed
+*/
+
+void print_number(int n)
+{
+	int temp;
+	int size = 1;
+
+	if (n == -2147483648)
+	{
+		_putchar(45);
+		_putchar(50);
+		n = n % 1000000000;
+		n = -n;
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		n = -n;
+	}
+
+	temp = n;
+
+	while (temp / 10 != 0)
+	{
+		temp = temp / 10;
+		size = size * 10;
+	}
+
+	for (; size > 0; size /= 10)
+	{
+		temp = n / size;
+		_putchar(temp + '0');
+		n = n % size;
+	}
+}
