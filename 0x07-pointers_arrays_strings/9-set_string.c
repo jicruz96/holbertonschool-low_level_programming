028de5e0f0e2bf44e7e95f84ff032e5288ef2d1e
jicruz96 <accounts@jicruz.com>
2020-02-25 02:48:56 +0000

    Debug ex. 8, 1st attempt: make betty approved

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* set_string: sets the value of a pointer to a char
+* set_string - sets the value of a pointer to a char
 *
 * @s: pointer that will be assigned to char
 * @to: pointer that points to char that s will be pointing to
