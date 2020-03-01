436e51578fcb3979618698831d1b3065f87c08e7
jicruz96 <accounts@jicruz.com>
2020-03-01 01:29:13 +0000

    Push folder 0x0A to repo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - prints name of file where program is saved
+*
+* @argc: number of arguments, always 1
+* @argv: pointer to file name as a string
+*
+* Return: Always 0 (success)
+*/
+
+int main (int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
