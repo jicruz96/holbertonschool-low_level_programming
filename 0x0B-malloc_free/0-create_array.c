3ab5d242908efd1d828a08cf12c2809a063e43e2
jicruz96 <accounts@jicruz.com>
2020-03-03 20:13:04 +0000

    Push folder 0x0B and ex. 0 to repo

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* create_array - creates a character array and initializes it
+*
+* @size: size of the character array
+* @c: character to add to array to initialize it
+*
+* Return: char pointer to the array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;	
+
+	size *= sizeof(c);		/*scale size to fit size of data type*/
+	array = malloc(size);		/*make array point to memory of size*/
+	for (i = 0; i < size; i++)	/*initialize array indexes with c*/
+		array[i] = c;
+	return (array);			/*return the array pointer*/
+}
