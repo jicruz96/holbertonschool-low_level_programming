b227eb7b72b67bb74a56677588fde95ab3e131f7
jicruz96 <accounts@jicruz.com>
2020-03-03 22:48:55 +0000

    Debug ex. 1, 7th attempt: simplify malloc expression; manually add 1 to string length var

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,9 +15,10 @@ char *_strdup(char *str)
 	int i, len;
 	char *ptr;
 
-	for (len = 0; str[len] != '\0'; len++) /* get string length */
+	for (len = 0; str[len]; len++) /* get string length */
 		;
-	ptr = (char *)malloc(len * sizeof(char));		/* allocate memory */
+	len++;
+	ptr = malloc(len);		/* allocate memory */
 	if (ptr == NULL)				/* check for NULL */
 		return (NULL);
 	for (i = 0; i <= len; i++)			/* copy string */
