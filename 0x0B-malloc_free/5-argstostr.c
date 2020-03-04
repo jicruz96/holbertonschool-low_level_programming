2fa81007b84b2c6631f1ec6e9f35afb08440d847
jicruz96 <accounts@jicruz.com>
2020-03-04 01:11:08 +0000

    Push ex. 5 to repo

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* argstostr - concatenates all the program's arguments into a string
+*
+* @ac: argument count
+* @av: argument vector
+*
+* Return: char pointer to string, or NULL if malloc fails, or if ac or av = 0
+*/
+
+char *argstostr(int ac, char **av)
+{
+	char *array;
+	int i, j, count;
+
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			count++;
+
+	array = malloc(count + 1);
+
+	for (i = 0; i < ac; i++)
+		{
+			array = str_concat(array, av[i]);
+			array = str_concat(array, "\n");
+		}
+
+	return (array);
+}
+
+char *str_concat(char *s1, char *s2)
+{
+        int len_s1;
+        int len_s2;
+        int i;
+        char *ptr;
+
+        for (len_s1 = 0; s1[len_s1]; len_s1++)
+                ;
+        for (len_s2 = 0; s2[len_s2]; len_s2++)
+                ;
+        if (len_s2 == 0 && len_s1 == 0)
+                ptr = malloc(0);
+        else
+                ptr = malloc(len_s1 + len_s2 + 1);
+        if (ptr == NULL)
+                return (NULL);
+        for (i = 0; i < len_s1; i++)
+                ptr[i] = s1[i];
+        for (i = 0; i <= len_s2; i++)
+                ptr[len_s1 + i] = s2[i];
+        return (ptr);
+}
