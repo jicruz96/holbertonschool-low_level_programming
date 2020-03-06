04f5dabfb2d9805037fb624ad40ce2da8e375d28
jicruz96 <accounts@jicruz.com>
2020-03-06 05:33:05 +0000

    Push ex. 3 to repo

--- 0x0C-more_malloc_free/debug_files/3-debug.c
+++ 0x0C-more_malloc_free/debug_files/3-debug.c
@@ -17,17 +17,28 @@ int *array_range(int min, int max)
 	int size = max - min + 1;	/* declare var size, to be used for memory */
 	int *p;				/* declare an int pointer. this shall be returned */
 
+	printf("setDigit = %d, size = %d, max = %d, min = %d\n", setDigit, size, max, min);
 
 	if (setDigit < 0)		/* Check if min > max. If so, return NULL */
+	{
+		printf("Min is greater than max! ERROR! Returning NULL\n");
 		return (NULL);
+	}
 
 	p = malloc(sizeof(int) * size);	/* Allocate memory */
 
+	printf("memory allocated!\n");
 	if (p == NULL)				/* Check if malloc failed */
+	{
+		printf("Malloc failed. Returning NULL\n");
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++, setDigit++)	/* Initialize from min to max */
+	{
 		p[i] = setDigit;
+		printf("Loop #%d: p[%d] = %d\n", i, i, p[i]);
+	}
 
 	return (p);
 }
