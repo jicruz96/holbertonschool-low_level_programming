6578e50131379a00b4b25ec9889bcf151c8a2e9f
jicruz96 <accounts@jicruz.com>
2020-03-06 01:41:24 +0000

    Push ex. 1 to repo

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include "holberton.h"
-
+#include <stdio.h>
 /**
 * string_nconcat - concatenates two strings
 *
@@ -13,5 +13,24 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	return (0);
+	unsigned int i, j, len;
+	char *p;					/* declare a char pointer */
+
+	for (i = 0, len = 1 + n; s1[i]; i++, len++)	/* Find memory size to allocate */
+		;
+
+	p = malloc(sizeof(char) * len); 		/* allocate memory */
+
+	if (p == NULL)					/* if malloc fails, return NULL */
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)				/* copy s1 onto p */
+		p[i] = s1[i];
+
+	for (j = 0; i < len && s2[j]; i++, j++)		/* copy up to n bytes of s2 onto p */
+		p[i] = s2[j];
+
+	p[i] = '\0';					/* make last position equal to null */
+
+	return (p);					/* return pointer */
 }
