ec3a15c4641ad941491b7600b6b6f6580f8c952a
jicruz96 <accounts@jicruz.com>
2020-03-06 04:21:12 +0000

    Push ex. 2 to repo

--- 0x0C-more_malloc_free/debug_files/2-debug.c
+++ 0x0C-more_malloc_free/debug_files/2-debug.c
@@ -17,14 +17,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;			/* declare a void pointer. we'll return this later */
 
 	if (nmemb == 0 || size == 0)	/* If either argument is zero, return NULL */
+	{
+		printf("An argument is zero, returning NULL!\n");
 		return (NULL);
+	}
 
 	p = malloc(size * nmemb);	/* allocate memory */
-
+	printf("p set to memory size = %d\n", size * nmemb);
 	if (p == NULL)			/* if malloc fails, return NULL */
+	{
+		printf("malloc failed, returning NULL!\n");
 		return (NULL);
+	}
 
 	memset(p, 0, size * nmemb);	/* Set memory to zero for all elements */
+	printf("all elements set to 0\n. Returning p...\n");
 
 	return (p);			/* Return the initialized, void pointer */
 }
