7ca6b90ee2940c24d623593586faa5465d14670d
jicruz96 <accounts@jicruz.com>
2020-03-10 00:24:18 +0000

    Push folder 0x0E and ex. 0 to repo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog - database for a dog's name, age, and owner
+*
+* @name: name of dog
+* @age: age of dog (in years)
+* @owner: name of owner of dog
+*
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
