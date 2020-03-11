dfdbd23bc28ce63959208795b85bb545048c6ebf
jicruz96 <accounts@jicruz.com>
2020-03-11 16:57:46 +0000

    Push initial files, without completed projects

---
+++ 0x0F-function_pointers/3/op_functions.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name, according to whatever printing function is called
+*
+* @name: name to print
+* @f: function that determines printing behavior
+*
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+}
