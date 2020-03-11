05a127444a5d7b4a0a740cf7d16fb381c8a1ca2b
jicruz96 <accounts@jicruz.com>
2020-03-11 21:35:36 +0000

    Push files associated with ex. 3 to repo

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "calc.h"
+#include <string.h>
+
+/**
+* get_op_func - selects the function requested by the user (for calc program)
+*
+* @s: operator. the operator will tell us which function to pick
+*
+* Return: pointer to the function that corresponds to s.
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	printf("Welcome to the get_op function. Entering while loop...\n");
+	while (i < 6)
+	{
+		printf("while loop entered! i = %d\n", i);
+		if (!(strcmp(ops[i].op,s)))
+		{
+			printf("Match found at index %d!\n", i);
+			return (ops[i].f);
+		}
+		else
+		{
+			printf("No match, incrementing i..\n");
+			i++;
+		}
+	}
+	printf("No matches found. Returning NULL\n");
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/debug_files/main_debug.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "calc.h"
+
+/**
+* main - performs simple operations on two numbers, prints result
+*
+* @argc: number of arguments. Should ALWAYS be 4. Else, return ERROR.
+* @argv: array of arguments.
+*
+* Return: 0 if successful |	Returns the following exit statuses if failed:
+*				 98: argc != 4
+*				 99: desired operation doesn't exist
+*				100: division by zero detected, ABORT!
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operation)(int, int) = get_op_func(argv[2]);
+
+	printf("Welcome to your main function!\n");
+	printf("Checking for errors...\n");
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("No errors found!\n");
+
+	a = atoi(*(argv + 1));
+	b = atoi(*(argv + 3));
+
+	printf("argc = %d, a = %d, b = %d, op = %s\n", argc, a, b, argv[2]);
+	printf("Checking for division by 0...\n");
+	if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("...we're good! Let's print the calculation:\n");
+	printf("%d\n", operation(a, b));
+
+	return (0);
+
+}
