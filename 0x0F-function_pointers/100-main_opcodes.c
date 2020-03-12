50f5dabe6a61f3a2b99db8cd03fcf345b3c1f2d2
jicruz96 <accounts@jicruz.com>
2020-03-12 04:49:29 +0000

    REVERT TO PREVIOUS VERSION OF EX. 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,7 +6,7 @@
 /**
 * main - prints its own opcodes
 *
-* @argc: number of arguments. Should ALWAYS be 2. Else, return ERROR.
+* @argc: number of arguments. Should ALWAYS be 1. Else, return ERROR.
 * @argv: array of arguments.
 *
 * Return: 0 if successful
@@ -14,20 +14,4 @@
 
 int main(int argc, char *argv[])
 {
-	int bytes;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
 }
