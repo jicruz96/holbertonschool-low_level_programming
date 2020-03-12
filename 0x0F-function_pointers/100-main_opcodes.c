21f7855ce97841a164a6ca0c6178609764e15fb7
jicruz96 <accounts@jicruz.com>
2020-03-12 02:20:33 +0000

    Push second version of ex. 100 file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "function_pointers.h"
+
+/**
+* main - prints its own opcodes
+*
+* @argc: number of arguments. Should ALWAYS be 1. Else, return ERROR.
+* @argv: array of arguments.
+*
+* Return: 0 if successful
+*/
+
+int main(int argc, char *argv[])
+{
+}
