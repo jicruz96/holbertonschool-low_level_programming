f47c222c29a00c947ccc0d87ea432c3f9fdd3495
jicruz96 <accounts@jicruz.com>
2020-03-14 23:47:27 +0000

    modify header to verify why the holberton checker is failing me

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -9,10 +8,5 @@ void print_char(va_list list);
 void print_str(va_list list);
 void print_int(va_list list);
 void print_float(va_list list);
-typedef struct printer_pick
-{
-	char format_type;
-	void (*printer)(va_list);
-} printer_pick;
 
 #endif
