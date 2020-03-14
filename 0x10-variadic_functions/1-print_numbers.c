90de18882c7b889ed58757b2020b4517badc2527
jicruz96 <accounts@jicruz.com>
2020-03-14 23:54:15 +0000

    Push ex. 1 to repo

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints numbers consecutively, separated by separator arg
+*
+* @separator: string to be printed between numbers
+* @n: number of arguments to print
+*
+* Return: void
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list numbers;
+
+	va_start(numbers, n);		/* initialize numbers list */
+
+	if (separator != NULL)		/* if separator is null, skip print code */
+	{
+		for (i = 0; i < n; i++)	/* print arguments */
+		{
+			printf("%d", va_arg(numbers, int));
+
+			if (i != n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+
+	}
+	printf("\n");			/* print newline AFTER end of print code */
+
+	va_end(numbers);		/* deallocate memory for numbers list */
+}
