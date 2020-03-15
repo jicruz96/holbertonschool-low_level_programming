9ca28f6d30736aecd39acf8c493bf5be0b434d8e
jicruz96 <accounts@jicruz.com>
2020-03-15 01:26:32 +0000

    Address what happens when separator is null on ex. 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,21 +18,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 
 	va_start(numbers, n);		/* initialize numbers list */
-
-	if (separator != NULL)		/* if separator is null, skip print code */
+	for (i = 0; i < n; i++)		/* print arguments */
 	{
-		for (i = 0; i < n; i++)	/* print arguments */
-		{
-			printf("%d", va_arg(numbers, int));
-
-			if (i != n - 1)
-			{
-				printf("%s", separator);
-			}
-		}
-
+		printf("%d", va_arg(numbers, int));
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");			/* print newline AFTER end of print code */
-
 	va_end(numbers);		/* deallocate memory for numbers list */
 }
