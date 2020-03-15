78f9f95bfb2f5d7e4bd3f44e06f779499402b82b
jicruz96 <accounts@jicruz.com>
2020-03-15 01:43:12 +0000

    Change va_start behavior on ex. 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,25 +18,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list strings;
 	char *null_check;
 
-	va_start(strings, n);
-
 	if (n != 0)
 	{
+		va_start(strings, n);
 		for (i = 0; i < n; i++)
 		{
 			null_check = va_arg(strings, char *);
-
 			if (null_check == NULL)
 				printf("(nil)");
 			else
 				printf("%s", null_check);
-
 			if (separator != NULL && i < (n - 1))
-			{
 				printf("%s", separator);
-			}
 		}
 		printf("\n");
-	}
 	va_end(strings);
+	}
 }
