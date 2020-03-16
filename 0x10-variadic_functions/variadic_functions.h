2a05cfb754309588700d86737aaac4f9b4786b61
jicruz96 <accounts@jicruz.com>
2020-03-16 16:52:34 +0000

    Make header file comply with style requirements

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,10 +6,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct printer_pick - associates a character with a print function
+ *
+ * @format_type: character type associated with a printer
+ * @printer: printing function associated with a character
+ */
+
 typedef struct printer_pick
 {
-        char format_type;
-        void (*printer)(va_list);
+	char format_type;
+	void (*printer)(va_list);
 } printer_pick;
 
 void print_char(va_list list);
