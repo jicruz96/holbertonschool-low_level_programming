2b56e5280841c0817ded306bb752eace908f0afe
jicruz96 <accounts@jicruz.com>
2020-03-19 20:53:42 +0000

    Push ex. 1 to repo

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,5 +11,26 @@
 
 size_t list_len(const list_t *h)
 {
+	/**
+	 * CODE BLOCK #0 - VARIABLES
+	 * n keeps track of the length of the list.
+	 * 	> n = 1 at first because all lists are at least 1.
+	 *	> n will be incremented each time we access a new element.
+	 */
+	size_t n = 1;
 
+	/**
+	 * CODE BLOCK #1 - COUNT LIST LENGTH
+	 * While there is a pointer ahead of us:
+	 * 	a. access the next element;
+	 * 	b. increment n;
+	 */
+	while (h->next != NULL)
+	{
+		h = h->next;				/*1a*/
+		n++;					/*1b*/
+	}
+
+	/* When done, return n */
+	return (n);
 }
