774946475ab8048bc6ded040a1588a386b7e497d
jicruz96 <accounts@jicruz.com>
2020-03-20 15:22:05 +0000

    Debug ex. 0, adjust behavior for when h == NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,17 +15,16 @@ size_t print_list(const list_t *h)
 	size_t n = 0;	/* n will store the number of elements we print */
 
 	/**
-	 * CODE BLOCK #1 - PRINT ALL BUT THE LAST ELEMENT
+	 * CODE BLOCK #1 - PRINT ALL ELEMENTS
 	 *
-	 * While there are elements left:
+	 * While the pointer you're at is not NULL:
 	 *	a. print the element you're pointing at;
 	 *		b. if the string is null, print [0] (nil) instead;
 	 *	c. then point to the next element;
 	 *	d. increment n to store how many elements you've printed;
 	 *
-	 * NOTE: THIS WILL NOT PRINT THE LAST ELEMENT. CODE BLOCK 2 DOES.
 	 */
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str != NULL)				/*1a*/
 			printf("[%u] %s\n", h->len, h->str);
@@ -34,19 +33,5 @@ size_t print_list(const list_t *h)
 		h = h->next;					/*1c*/
 		n++;						/*1d*/
 	}
-
-	/**
-	 * CODE BLOCK #2 - PRINT THE LAST ELEMENT & RETURN
-	 *
-	 * a. Print the last element;
-	 *	b. if the string is null, print [0] (nil) instead;
-	 * c. then increment n once more to record the print;
-	 * d. We're done! Return n.
-	 */
-	if (h->str != NULL)					/*2a*/
-		printf("[%u] %s\n", h->len, h->str);
-	else							/*2b*/
-		printf("[0] (nil)\n");
-	n++;							/*2c*/
 	return (n);						/*2d*/
 }
