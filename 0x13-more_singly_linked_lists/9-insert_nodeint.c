575fad7d0a0dd40b5f8c4390cc102657ecf45b5b
jicruz96 <accounts@jicruz.com>
2020-03-24 22:47:47 +0000

    Debug ex. 9, first attempt: Address case when idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,18 +18,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new == NULL || head == NULL)
 		return (NULL);
-
 	tmp = *head;
 	new->n = n;
-
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i + 1 < idx; i++)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
 	}
-
-	new->next = tmp->next;
-	tmp->next = new;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = tmp->next;
+		tmp->next = new;
+	}
 	return (new);
 }
