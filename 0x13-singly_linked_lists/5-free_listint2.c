4be72add7f84a0c3f7ca336ad51193385960adc5
jicruz96 <accounts@jicruz.com>
2020-03-24 05:50:15 +0000

    Push alternate version of ex. 5 to repo

--- 0x13-singly_linked_lists/5-free_listint2.c
+++ 0x13-singly_linked_lists/5-free_listint2.c
@@ -11,19 +11,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp1, *tmp2;
+	listint_t *tmp;
 
 	if (head == NULL || *head == NULL)
 		return;
 
-	tmp1 = *head;
-	*head = NULL;
-
-	while (tmp1 != NULL)
+	while (*head != NULL)
 	{
-		tmp2 = tmp1->next;
-		free(tmp1);
-		tmp1 = tmp2;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
 	
 }
