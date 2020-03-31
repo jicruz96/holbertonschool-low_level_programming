eb15f66304e32dd0e0f060065a5c9fb3aa555a09
jicruz96 <accounts@jicruz.com>
2020-03-31 10:32:59 +0000

    Make ex. 100 comply with style requirements, for real this time

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 
 		if (s2[1] == '*' || s1[0] == '\0')
 			return (wildcmp(s1, s2 + 1));
-		
+
 		if (s2[1] == s1[0])
 			if (wildcmp(s1 + 1, s2 + 2))
 				return (1);
