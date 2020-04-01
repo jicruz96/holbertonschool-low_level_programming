ea1e10a6a32aa18faffe3e67db6f6825e142e5c6
jicruz96 <accounts@jicruz.com>
2020-04-01 04:35:57 +0000

    Add _strlen function prototype to holberton.h

--- 0x15-file_io/main_files/holberton.h
+++ 0x15-file_io/main_files/holberton.h
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
+int _strlen(char *string);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
