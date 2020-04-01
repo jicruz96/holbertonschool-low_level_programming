2d999eed96acec9ef1dd980d2d2d5e661eb9dda6
jicruz96 <accounts@jicruz.com>
2020-04-01 06:50:39 +0000

    Make multiple edits to ex. 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,30 +14,40 @@ int main(int argc, char *argv[])
 	int file_from, file_to, sz = 1, check;
 	char buf[1024];
 
-	if (argc != 3)	/* Check if argc = 3, if not, exit 97 */
+	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
-	file_from = open(argv[1], O_RDONLY);	/* Open file to read from */
-	if (file_from == -1)			/* Exit 98 if can't open */
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
-	/* Open file to copy to */
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664); /*Open copyfile*/
-	if (file_to == -1)	/* Exit 98 if can't open */
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while (sz)
 	{
 		sz = read(file_from, buf, 1024);
+		if (sz == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		check = write(file_to, buf, sz);
 		if (check == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-
 	check = close(file_from);
 	if (check == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	check = close(file_to);
 	if (check == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to)
+		exit(100);
+	}
 	return (0);
 }
