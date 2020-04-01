26a6f97565a2b206438c6785a493e6597b37e108
jicruz96 <accounts@jicruz.com>
2020-04-01 04:49:40 +0000

    Push ex. 100 to repo

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - displays information contained in the ELF header
+ *
+ * @argc: argument count (should always be 3)
+ * @argv: array of arguments
+ *
+ * Return: number of letters printed | 0 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	dprintf(STDERR_FILENO, "Usage: %s elf_header elf_filename\n", argv[0]);
+	exit(98);	
+	return (0);
+}
