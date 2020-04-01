b027d6fe0b757bc992ca23cceff2510e376e5edf
jicruz96 <accounts@jicruz.com>
2020-04-01 04:53:18 +0000

    Make ex. 100 comply with style requirements

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,15 +3,15 @@
 /**
  * main - displays information contained in the ELF header
  *
- * @argc: argument count (should always be 3)
- * @argv: array of arguments
+ * @arc: argument count (should always be 3)
+ * @arv: array of arguments
  *
  * Return: number of letters printed | 0 if error
  */
 
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(int arc __attribute__((unused)), char *arv[] __attribute__((unused)))
 {
 	dprintf(STDERR_FILENO, "Usage: %s elf_header elf_filename\n", argv[0]);
-	exit(98);	
+	exit(98);
 	return (0);
 }
