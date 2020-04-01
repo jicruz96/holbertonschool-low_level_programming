82e29faf0485371f8765e3baef4faf0789867b1d
jicruz96 <accounts@jicruz.com>
2020-04-01 23:49:06 +0000

    Debug ex. 100, second attempt: change behavior for NULL input edge case

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,7 +12,7 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *nextNode, *newHead;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
 	if ((*head)->next == NULL)
