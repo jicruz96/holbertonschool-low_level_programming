6550e64178070a4c7f6a9d6d07c479e73ae2bab0
jicruz96 <accounts@jicruz.com>
2020-04-02 19:30:00 +0000

    Push ex. 102 to repo

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_listint_safe - frees a linked list; avoids infinite loops
+ *
+ * @head: head pointer to list to be printed
+ *
+ * Return: the number of nodes
+ */
+
+void free_listint_safe(listint_t **head)
+{
+	int n = 0, tmp = 1, hops = 1;
+	listint_t *tortoise, *hare;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	for (tortoise = *head; hops == tmp; tmp++, hops++, tortoise = *head)
+	{
+		hare = *head;
+
+		while (hare && hops--)
+			hare = hare->next;
+
+		if (hare == NULL)
+		{
+			tmp++;
+			break;
+		}
+
+		for (hops = 0; tortoise != hare; hops++)
+			tortoise = tortoise->next;
+	}
+
+	for (; head && --tmp; n++)
+	{
+		tortoise = (*head)->next;
+		free(*head);
+		if (tmp == 1)
+			*head = NULL;
+		else
+			*head = tortoise;
+	}
+}
