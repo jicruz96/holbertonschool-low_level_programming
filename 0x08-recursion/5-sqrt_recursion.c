3f5615dbfa1cbda4d84a0bf9f7052c61ee5c0da5
jicruz96 <accounts@jicruz.com>
2020-04-04 04:51:34 +0000

    Push new and improved ex. 5 version to repo

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,20 +10,39 @@
 
 int _sqrt_recursion(int n)
 {
-	int guess1, guess2;
+	int check;
+
+	if (n == 1)
+		return (1);
 
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n < 2)
-	{
-		return (n);
-	}
-	else
-	{
-		guess1 = _sqrt_recursion(n - 1);
-		guess2 = (guess1 + (n / guess1)) / 2;
-		return (guess2);
-	}
+
+	check = sqrt_search(0, n / 2, n);
+
+	if (check * check == n)
+		return (check);
+
+	return (-1);
+}
+/**
+ * sqrt_search - finds a guess for the square root of n
+ * @low: low-ball guess, usually zero
+ * @high: high-ball guess, usually no greater than n / 2
+ * @n: number to find square root for
+ * Return: guess of square root
+ */
+int sqrt_search(int low, int high, int n)
+{
+	int guess = (low + high) / 2;
+	int next = guess + 1, prev = guess - 1;
+	int guess_squared = guess * guess, next_squared = next * next;
+
+	if (low > high)
+		return (low);
+	if ((guess_squared <= n) && (next_squared > n))
+		return (guess);
+	if (guess_squared < n)
+		return (sqrt_search(next, high, n));
+	return (sqrt_search(low, prev, n));
 }
