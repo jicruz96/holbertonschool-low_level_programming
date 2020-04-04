f6afaaa6123fd4c99561f6d68359c2dee1d5c720
jicruz96 <accounts@jicruz.com>
2020-04-04 06:54:39 +0000

    Remove unnecesary lines from ex. 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,13 +1,9 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
 * is_palindrome - returns 1 if string is palindrome, 0 if not
-*
 * @s: string to be checked
-*
 * Return: 1 if palindrome, 0 otherwise
 */
-
 int is_palindrome(char *s)
 {
 	int check;
@@ -15,14 +11,11 @@ int is_palindrome(char *s)
 
 	if (s[0] == '\0')
 		return (1);
-
 	if ((s[1] > 31 || s[1] < 127))
 		LastLetter = findLastLetter(s + 1);
 	else
 		LastLetter = s;
-
 	return (check = palindrome_check(s, LastLetter));
-
 }
 /**
  * findLastLetter - returns last letter in a string
@@ -33,7 +26,6 @@ char *findLastLetter(char *s)
 {
 	if (s[1] <= 31 || s[1] >= 127)
 		return (s);
-
 	return (findLastLetter(s + 1));
 }
 /**
@@ -48,6 +40,5 @@ int palindrome_check(char *s, char *LastLetter)
 		return (0);
 	if (s == LastLetter || s + 1 == LastLetter)
 		return (1);
-
 	return (palindrome_check(s + 1, LastLetter - 1));
 }
