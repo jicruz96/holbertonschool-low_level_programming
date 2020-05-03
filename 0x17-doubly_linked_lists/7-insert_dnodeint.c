9ae8f04a15c1890086778dae641ae81197955bbc
jicruz96 <accounts@jicruz.com>
2020-05-03 00:43:27 +0000

    Ensure 7-insert_dnodeint.c can insert node at index 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,34 +12,57 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	dlistint_t *ptr = *h;
 
+	/**
+	 * EDGE CASES - Check if:
+	 * 1. malloc failed 
+	 * OR
+	 * 2. node is to be inserted as non-first node in an empty list
+	 **/
 	if (new == NULL || (*h == NULL && idx))
 		return (NULL);
 
+	/* Create node */
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
 
-	if (*h == NULL)
+	/* EDGE CASE: If idx is 0, set new->next to head and head to new */
+	if (idx == 0)
 	{
+		if (*h)
+		{
+			new->next = *h;
+			(*h)->prev = new;
+		}
 		*h = new;
 		return (new);
 	}
 
-	while (ptr->next && --idx)
+	/* Traverse to index OR end of list */
+	while (--idx && ptr->next)
 		ptr = ptr->next;
 
+	/**
+	 * EDGE CASE
+	 * 	If at end of list, but not at index, insertion is impossible.
+	 * 	Free new and return
+	 **/
 	if (idx)
 	{
 		free(new);
 		return (NULL);
 	}
 
+	/* if there's another node, have new point to it and have it point to new */
 	if (ptr->next)
 	{
 		new->next = ptr->next;
 		(ptr->next)->prev = new;
 	}
+
+	/* Set new->prev to current pointer and current pointer's next to new */
 	new->prev = ptr;
 	ptr->next = new;
+
 	return (new);
 }
