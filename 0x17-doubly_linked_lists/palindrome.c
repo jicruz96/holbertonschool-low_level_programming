e4bedd9dafdad6554e312664086766287805fe6f
jicruz96 <accounts@jicruz.com>
2020-05-03 09:45:24 +0000

    Refactor palindrome.c | add prototypes to lists.h

--- 0x17-doubly_linked_lists/palindrome.c
+++ 0x17-doubly_linked_lists/palindrome.c
@@ -1,25 +1,19 @@
 #include "lists.h"
 
-int is_palindrome(unsigned int n);
-dlistint_t *num2dlistint(unsigned int n);
-
 /**
  * main - finds largest palindrome that's a product of 3 digit numbers
  * Return: always 1
  **/
 int main(void)
 {
-	int a = 100, b = 100;
-	int largest_palindrome = 0;
+	int a, b, c;
 
 	for (a = 100; a < 1000; a++)
 		for (b = a; b < 1000; b++)
 			if (is_palindrome(a * b))
-				if (a * b > largest_palindrome)
-					largest_palindrome = a * b;
-
-	printf("LARGEST PALINDROME: %u\n", largest_palindrome);
-	return (1);
+				if (a * b > c)
+					c = a * b;
+	return (printf("%u\n", c));
 }
 
 /**
@@ -29,30 +23,21 @@ int main(void)
  **/
 int is_palindrome(unsigned int n)
 {
-	dlistint_t *h = NULL, *t;
-	int result = 1;
+	dlistint_t *h = NULL, *t = NULL, *tmp;
 
-	if (!n)
-		return (result);
-
-	for (; n; n /= 10)
-		add_dnodeint_end(&h, n % 10);
-
-	for (t = h; t->next;)
-		t = t->next;
-
-	while (t->n == h->n && h != t)
+	if (n)
 	{
-		t = t->prev;
-		if (t == h)
-			break;
-		h = h->next;
+		for (; n; n /= 10)
+			add_dnodeint_end(&h, n % 10);
+		for (t = h; t->next; t = t->next)
+			;
+		for (tmp = h; h->n == t->n && h != t;)
+		{
+			h = h->next;
+			if (h != t)
+				t = t->prev;
+		}
+		free_dlistint(tmp);
 	}
-
-	if (t != h)
-		result--;
-
-	free_dlistint(h);
-
-	return (result);
+	return (t == h);
 }
