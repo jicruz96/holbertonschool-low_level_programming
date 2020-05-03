1e0d5e657bcfa88e39e4cc8d4033e94b8031eb0f
jicruz96 <accounts@jicruz.com>
2020-05-03 00:48:08 +0000

    Ensure file 7 complies with style requirements

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,21 +12,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	dlistint_t *ptr = *h;
 
-	/**
-	 * EDGE CASES - Check if:
-	 * 1. malloc failed 
-	 * OR
-	 * 2. node is to be inserted as non-first node in an empty list
-	 **/
+	/* Check if malloc failed OR if list doesn't exist and idx is not 0 */
 	if (new == NULL || (*h == NULL && idx))
 		return (NULL);
-
 	/* Create node */
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-
-	/* EDGE CASE: If idx is 0, set new->next to head and head to new */
+	/* If idx is 0, set new->next to head and head to new */
 	if (idx == 0)
 	{
 		if (*h)
@@ -37,32 +30,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (new);
 	}
-
 	/* Traverse to index OR end of list */
 	while (--idx && ptr->next)
 		ptr = ptr->next;
-
-	/**
-	 * EDGE CASE
-	 * 	If at end of list, but not at index, insertion is impossible.
-	 * 	Free new and return
-	 **/
+	/* If at end of list, but not at index, insertion is impossible. */
 	if (idx)
 	{
 		free(new);
 		return (NULL);
 	}
-
 	/* if there's another node, have new point to it and have it point to new */
 	if (ptr->next)
 	{
 		new->next = ptr->next;
 		(ptr->next)->prev = new;
 	}
-
 	/* Set new->prev to current pointer and current pointer's next to new */
 	new->prev = ptr;
 	ptr->next = new;
-
 	return (new);
 }
