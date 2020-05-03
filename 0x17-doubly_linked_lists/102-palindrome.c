434059f6fcf8b1608349592a1df700b08df24ca4
jicruz96 <accounts@jicruz.com>
2020-05-03 20:20:25 +0000

    rename palindrome.c to 102-palindrome.c; refactor 102-palindrome.c

---
+++ 0x17-doubly_linked_lists/102-palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#define MAX 1000
+/**
+ * main - finds largest palindrome that's a product of 3 digit numbers
+ * Return: always 1
+ **/
+int main(void)
+{
+	int a, b, c = 0;
+
+	for (a = MAX / 10 + 1; a < MAX; a++)
+		for (b = a; b < MAX; b++)
+			if (a * b > c && is_palindrome(a * b))
+				c = a * b;
+	return (printf("%d\n", c));
+}
+
+/**
+ * is_palindrome - returns 1 if n is palindrome | 0 otherwise
+ * @n: number to check
+ * Return: 1 if palindrome | 0 otherwise
+ **/
+int is_palindrome(unsigned int n)
+{
+	dlistint_t *h = NULL, *t = NULL, *tmp = NULL;
+	int result = 1, check;
+
+	for (check = 0; n; n /= 10, check++)
+		t = add_dnodeint_end(&h, n % 10);
+	for (tmp = h, check /= 2; check && result; h = h->next, t = t->prev, check--)
+		if (h->n != t->n)
+			result = 0;
+	free_dlistint(tmp);
+	return (result);
+}
