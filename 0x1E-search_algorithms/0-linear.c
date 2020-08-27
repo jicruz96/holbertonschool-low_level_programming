7c137039dca0550844449844cfb4c57d20d3de5f
jicruz96 <accounts@jicruz.com>
2020-08-27 00:25:38 +0000

    Initial commit of folder 0x1E

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search algorithm
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to search for
+ * Return: index where value is first found || -1 if not found or NULL array
+ **/
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	if (array)
+		for (i = 0; i < size; i++)
+			if (array[i] != value)
+				printf("Value checked array[%u] = [%d]\n", i, array[i]);
+			else
+				return ((int)i);
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,4 @@
+<h1>0x1E. C - Search Algorithms</h1>
+<h6>by J.I.</h6>
+
+This directory contains programs associated with the<strong> 0x1E. C - Search Algorithms </strong>project of the Higher Level Programming section of the Holberton School Foundations curriculum.
